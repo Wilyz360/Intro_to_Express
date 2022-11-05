4afb89205fba1f4bcb490f2fb637d8b3318082ef
ElmoMax <williamzhicay32@gmail.com>
2022-11-04 20:43:25 -0400

    Intro to Express Part 2

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express'); // API framework for http
+const morgan = require('morgan'); // A middleware library is used for logging
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express(); // resturn a express server application
+app.use(morgan('dev')); // insert morgan middleware - morgan('dev') use developer version - hadle login request information
+
+// Setup express to serve files from the public folder with the help of middleware function
+app.use(express.static(__dirname + '/public')); // __dirname is a special variable in node - it will refer to the absolute path of the current directory the file is in
+
 
 // Middleware Function
 // Setup a req and res method
